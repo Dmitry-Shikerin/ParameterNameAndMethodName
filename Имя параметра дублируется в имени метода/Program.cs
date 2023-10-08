76d79e87a0b05457d0274a47292584130fce69ad
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2023-10-08 15:16:43 +0300

    Добавьте файлы проекта.

---
+++ Имя параметра дублируется в имени метода/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Имя_параметра_дублируется_в_имени_метода
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public void Shoot(Player player) { }
+        public string Find(int index) { }
+    }
+}
